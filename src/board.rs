7aea2fcf67f5398c2a7b50955dd50a438c8624b8
Conor Lindahl <conor.lindahl@gmail.com>
2022-09-04 01:43:34 -0500

    Reduce board logic

--- src/board.rs
+++ src/board.rs
@@ -9,19 +9,7 @@ pub struct Board {
 impl Board {
     pub fn get_square(&self, file: usize, row: usize) -> Square {
         self.squares[file][row]
-    }
 
-    pub fn get_file(&self, file: usize) -> [Square; 8] {
-        let mut resultFile: [Square; 8] = [Square{rank: Rank::R1, file: File::A}; 8];
 
-        for i in 0..8 {
-            resultFile[i] = self.squares[i][file];
-        }
-
-        resultFile
-    }
-
-    pub fn get_row(&self, row: usize) -> [Square; 8] {
-        self.squares[row]
     }
 }
